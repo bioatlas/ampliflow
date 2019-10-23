94aa51b35ab29c35de2593ed31d2a2e4202aaa04
DiegoBrambilla <diego.brambilla@lnu.se>
2019-10-23 15:45:34 +0200

    temporary, simplified version of scrape_software_versions.py

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -10,12 +10,12 @@ regexes = {
     'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
     'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
     'Cutadapt': ['v_cutadapt.txt', r"(\S+)"],
-    'dada2filter.R': ['v_dada2filter.txt', r"dada2filter version (\S+)"]
-    'dada2errmodels.R': ['v_dada2errmodels.txt', r"dada2errmodels version (\S+)"]
-    'dada2cleanNmerge.R': ['v_dada2cleanNmerge.txt', r"dada2cleanNmerge version (\S+)"]
-    'dada2bimeras.R': ['v_dada2bimeras.txt', r"dada2bimeras version (\S+)"]
-    'dada2idseq.R': ['v_dada2idseq.txt', r"dada2idseq version (\S+)"]
-    'dada2taxonomy.R': ['v_.txt', r"dada2taxonomy version (\S+)"]
+#    'dada2filter.R': ['v_dada2filter.txt', r"dada2filter version (\S+)"]
+#    'dada2errmodels.R': ['v_dada2errmodels.txt', r"dada2errmodels version (\S+)"]
+#    'dada2cleanNmerge.R': ['v_dada2cleanNmerge.txt', r"dada2cleanNmerge version (\S+)"]
+#    'dada2bimeras.R': ['v_dada2bimeras.txt', r"dada2bimeras version (\S+)"]
+#    'dada2idseq.R': ['v_dada2idseq.txt', r"dada2idseq version (\S+)"]
+#    'dada2taxonomy.R': ['v_.txt', r"dada2taxonomy version (\S+)"]
 }
 results = OrderedDict()
 results['bioatlas/ampliflow'] = '<span style="color:#999999;\">N/A</span>'
@@ -23,12 +23,12 @@ results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
 results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
 results['Cutadapt'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2filter.R'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2errmodels.R'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2cleanNmerge.R'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2bimeras.R'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2idseq.R'] = '<span style="color:#999999;\">N/A</span>'
-results['dada2taxonomy.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2filter.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2errmodels.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2cleanNmerge.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2bimeras.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2idseq.R'] = '<span style="color:#999999;\">N/A</span>'
+#results['dada2taxonomy.R'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
 for k, v in regexes.items():
